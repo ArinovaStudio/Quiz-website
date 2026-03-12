4cddb4a76949863e2dfea88b79131952417859ff
OM BHARGAV <ombhargav788@gmail.com>
2026-03-12 19:17:44 +0530

    added changes

--- app/mobile/(user)/(navigation)/(home)/_components/CategorySection.tsx
+++ app/mobile/(user)/(navigation)/(home)/_components/CategorySection.tsx
@@ -5,24 +5,28 @@ import CustomSwiper from "@/components/CustomSwiper";
 import ErrorLoading from "@/components/ErrorLoading";
 import { colorMap } from "@/lib/constants";
 import { fetcher } from "@/lib/fetcher";
+import { ArrowRight } from "lucide-react";
 import useSWR from "swr";
-
 interface Props {
   selected: string;
   setSelected: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export default function CategorySection({ selected, setSelected }: Props) {
-  const { data, isLoading, error } = useSWR("/api/categories/", fetcher,{revalidateOnFocus: false,revalidateIfStale: false});
+  const { data, isLoading, error } = useSWR("/api/categories/", fetcher, {
+    revalidateOnFocus: false,
+    revalidateIfStale: false,
+  });
   const colors = Object.keys(colorMap);
-  const categories = data?.categories ?? []
+  const categories = data?.categories?.slice(0,5) ?? [];
   return (
     <div className="space-y-3 sm:space-y-4 px-1 sm:px-0">
-      
       {/* Header */}
       <div className="flex uppercase text-lg sm:text-2xl font-extrabold justify-between items-center">
         <div className="tracking-wide">categories</div>
-        <div className="text-xs text-primary"><Link href={"/mobile/categories"}>View ALl Categories</Link></div>
+        <div className="text-xs text-primary">
+          <Link href={"/mobile/categories"}>View ALl Categories</Link>
+        </div>
       </div>
 
       {/* Swiper */}
@@ -39,35 +43,46 @@ export default function CategorySection({ selected, setSelected }: Props) {
         >
           <CustomSwiper>
             {categories &&
-              [{ name: "defaulttype" }, ...categories].map(
-                (cat: any, index: number) =>
-                  cat.name === "defaulttype" ? (
-                    <CategoryCard
-                      key="all"
-                      name="all"
-                      image={cat?.image}
-                      onClick={() => setSelected("all")}
-                      id={"all"}
-                      selected={selected === "all"}
-                      color={colors[2]}
-                      eventsCount={categories.reduce(
-                        (acc: number, item: any) =>
-                          acc + (item?.tournamentsSize ?? 0),
-                        0
-                      )}
-                    />
-                  ) : (
-                    <CategoryCard
-                      key={cat.id}
-                      id={cat.id}
-                      selected={selected === cat.id}
-                      onClick={() => setSelected(cat.id)}
-                      name={cat.name}
-                      image={cat.image}
-                      eventsCount={cat?.tournamentsSize}
-                      color={colors[index % colors.length]}
-                    />
-                  )
+              [
+                { name: "defaulttype" },
+                ...categories,
+                { name: "view_all_123" },
+              ].map((cat: any, index: number) =>
+                cat.name === "defaulttype" ? (
+                  <CategoryCard
+                    key="all"
+                    name="all"
+                    image={cat?.image}
+                    onClick={() => setSelected("all")}
+                    id={"all"}
+                    selected={selected === "all"}
+                    color={colors[2]}
+                    eventsCount={categories.reduce(
+                      (acc: number, item: any) =>
+                        acc + (item?.tournamentsSize ?? 0),
+                      0
+                    )}
+                  />
+                ) : cat.name === "view_all_123" ? (
+                  <Link href={"/mobile/categories"}>
+                    <div className="shadow-sm border h-full rounded-lg bg-background flex items-center justify-center">
+                      <span className="text-primary items-center gap-1 flex text-sm">
+                        View All <ArrowRight size={17} />
+                      </span>
+                    </div>
+                  </Link>
+                ) : (
+                  <CategoryCard
+                    key={cat.id}
+                    id={cat.id}
+                    selected={selected === cat.id}
+                    onClick={() => setSelected(cat.id)}
+                    name={cat.name}
+                    image={cat.image}
+                    eventsCount={cat?.tournamentsSize}
+                    color={colors[index % colors.length]}
+                  />
+                )
               )}
           </CustomSwiper>
         </ErrorLoading>
